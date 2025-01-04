aad140c88bc3176b0168ec06c6cb7ccfdd24870c
IkaroJS <Ikarojesusdasilva@outlook.com>
2025-01-04 18:21:25 -0300

    Implementação do Pico

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Pizzaria redondinha</title>
 </head>
 
 <body>
     <header>
         <div>
-            <h1>Pizzaria redondinha</h1>            
+            <img src="img/Icon pizza.png" alt="logo pizza">
+            <h1>Pizzaria redondinha</h1>
         </div>
     </header>
+
+    <script src="js/script.js"></script>
 </body>
 
 </html>
